65f22f223eeb6a110358d4b8dac5e8f7bc4c978d
Jingwiw <wangjingwei@iscas.ac.cn>
2026-07-17 13:49:53 +0800

    chore: bootstrap Rust project
    Signed-off-by: Jingwiw <wangjingwei@iscas.ac.cn>

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: (C) 2026 Institute of Software, Chinese Academy of Sciences (ISCAS)
+// SPDX-FileCopyrightText: (C) 2026 openRuyi Project Contributors
+// SPDX-FileContributor: Jingwiw <wangjingwei@iscas.ac.cn>
+//
+// SPDX-License-Identifier: MulanPSL-2.0
