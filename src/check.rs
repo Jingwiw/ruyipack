c3ecbeca1ad6f4ad18599a07ef96dd90f2a04448
Jingwiw <wangjingwei@iscas.ac.cn>
2026-09-16 20:23:24 +0800

    docs(check): note pending URL policy review
    Signed-off-by: Jingwiw <wangjingwei@iscas.ac.cn>

--- src/check.rs
+++ src/check.rs
@@ -14,6 +14,8 @@ use crate::{
     spec::ParsedSpec,
 };
 
+// TODO: Confirm URL-less package policy with openRuyi before changing RPM015.
+// https://github.com/openRuyi-Project/openRuyi/issues/1227
 const REQUIRED_TAG_LINT_IDS: [&str; 6] =
     ["RPM010", "RPM011", "RPM012", "RPM013", "RPM014", "RPM015"];
 
